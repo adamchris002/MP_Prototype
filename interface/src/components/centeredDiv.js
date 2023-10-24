b10d01e1c1c92b70c6b5f888f834a54bf229b936
Adam Christoper <adam2510025@gmail.com>
2023-10-24 15:19:02 +0700

    add new content in landing page and centered div component

---
+++ interface/src/components/centeredDiv.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { styled } from '@mui/system';
+
+const DivFlexCenter = styled('div')((props) =>({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent:'center',
+}));
+
+export default DivFlexCenter;
--- interface/src/pages/landingPage.js
+++ interface/src/pages/landingPage.js
@@ -4,6 +4,7 @@ import { Box, ImageList, ImageListItem, Typography } from "@mui/material";
 import SmallCarousel from "../components/smallCarousel";
 import Section from "../components/section";
 import MyFooter from "../components/footer";
+import DivFlexCenter from "../components/centeredDiv";
 
 const LandingPage = () => {
   const dummyApplication = [
@@ -15,8 +16,11 @@ const LandingPage = () => {
     "https://assets-a1.kompasiana.com/items/album/2022/12/22/screenshot-847-63a3d1e608a8b54fcb77b6f2.png",
   ];
 
+  const repoCount = ["", "", "", ""];
 
   const [hoveredItem, setHoveredItem] = useState(null);
+  const [repoHoveredItem, setRepoHoveredItem] = useState(null);
+  const [miscHoveredItem, setMiscHoveredItem] = useState(null);
 
   const handleMouseEnter = (index) => {
     setHoveredItem(index);
@@ -25,6 +29,23 @@ const LandingPage = () => {
   const handleMouseLeave = () => {
     setHoveredItem(null);
   };
+
+  const handleRepoMouseEnter = (index) => {
+    setRepoHoveredItem(index);
+  };
+
+  const handleRepoMouseLeave = () => {
+    setRepoHoveredItem(null);
+  };
+
+  const handleMiscMouseEnter = (index) => {
+    setMiscHoveredItem(index);
+  };
+
+  const handleMiscMouseLeave = () => {
+    setMiscHoveredItem(null);
+  };
+
   return (
     <>
       <div
@@ -50,51 +71,33 @@ const LandingPage = () => {
           <br />
           {"(Paul)"}
         </Typography>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "140px",
-            flexDirection: "column",
-          }}
-        >
-          <Box
-            style={{ objectFit: "fill" }}
-            component="img"
-            height="25%px"
-            width="50%"
-            src="https://www.birds.cornell.edu/home/wp-content/uploads/2023/09/334289821-Baltimore_Oriole-Matthew_Plante.jpg"
-          />
-          <Typography
-            style={{ marginTop: "221px", marginBottom: "78px" }}
-            fontSize={"36px"}
-          >
-            Digital Collection
-          </Typography>
-        </div>
       </div>
+      <DivFlexCenter style={{ marginTop: "140px" }}>
+        <Box
+          style={{ objectFit: "fill" }}
+          component="img"
+          height="25%px"
+          width="50%"
+          src="https://www.birds.cornell.edu/home/wp-content/uploads/2023/09/334289821-Baltimore_Oriole-Matthew_Plante.jpg"
+        />
+      </DivFlexCenter>
+      <DivFlexCenter>
+        <Typography
+          style={{ marginTop: "221px", marginBottom: "78px" }}
+          fontSize={"36px"}
+        >
+          Digital Collection
+        </Typography>
+      </DivFlexCenter>
       <Section>
         <SmallCarousel />
       </Section>
-      <div
-        style={{
-          marginTop: "168px",
-          marginBottom: "80px",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <DivFlexCenter style={{ marginTop: "168px", marginBottom: "80px" }}>
         <Typography fontSize={36} fontWeight={500}>
           Educational Aplication
         </Typography>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      </DivFlexCenter>
+      <DivFlexCenter>
         <Box
           style={{
             width: "100%",
@@ -136,10 +139,71 @@ const LandingPage = () => {
             ))}
           </ImageList>
         </Box>
+      </DivFlexCenter>
+      <DivFlexCenter style={{ paddingTop: "164px" }}>
+        <Typography fontSize={36}>
+          Repository & Free Certified E-Courses
+        </Typography>
+      </DivFlexCenter>
+      <DivFlexCenter>
+        {repoCount.map((item, index) => {
+          return (
+            <div
+              index={index}
+              onMouseEnter={() => {
+                handleRepoMouseEnter(index);
+              }}
+              onMouseLeave={handleRepoMouseLeave}
+              style={{
+                width: "208px",
+                height: "170px",
+                background: "#D9D9D9",
+                boxShadow:
+                  repoHoveredItem === index
+                    ? "0px 0px 10px 2px rgba(0, 0, 0, 0.5)"
+                    : "",
+                marginRight: "42px",
+                borderRadius: "5px",
+                marginTop: "40px",
+                transition: "box-shadow 0.3s",
+              }}
+            ></div>
+          );
+        })}
+      </DivFlexCenter>
+      <div style={{ margin: "153px 214px" }}>
+        <Typography fontSize={23} align="justify">
+          {`If you need scientific reference materials, you can submit a request through the respective Program or Faculty lecturers. The library materials that can be proposed includee books, journals, multimedia materials, or other scientific materials that are not yet available in Atmalib or updated editions of existing titles in Atmalib. The more complete information you provide about the proposed materials, the greater the chance that the materials we provide will meet your needs. Please also mention the purpose of the materials (e.g., for learning, research, or community service). if applicable, please provide the name or course code. then, the request can be forwarded by the Program and Faculty to the Library through the Service Request menu.`}
+        </Typography>
       </div>
+      <DivFlexCenter>
+        {repoCount.map((item, index) => {
+          return (
+            <div
+              index={index}
+              onMouseEnter={() => {
+                handleMiscMouseEnter(index);
+              }}
+              onMouseLeave={handleMiscMouseLeave}
+              style={{
+                width: "188px",
+                height: "109px",
+                background: "#D9D9D9",
+                marginRight: "38px",
+                borderRadius: "5px",
+                transition: "box-shadow 0.3s",
+                boxShadow:
+                  miscHoveredItem === index
+                    ? "0px 0px 10px 2px rgba(0, 0, 0, 0.5)"
+                    : "",
+              }}
+            ></div>
+          );
+        })}
+      </DivFlexCenter>
       <MyFooter />
     </>
   );
 };
 
-export default LandingPage
+export default LandingPage;
